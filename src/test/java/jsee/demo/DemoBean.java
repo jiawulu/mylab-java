205031d00011b1e2be6ed62d1741d475776f6477
luu <jiawu.lu@gmail.com>
2013-08-03 07:16:05 +0800

    update it

---
+++ src/test/java/jsee/demo/DemoBean.java
@@ -0,0 +1,14 @@
+package jsee.demo;
+
+public class DemoBean {
+    private int counter;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+}
